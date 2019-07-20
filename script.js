1030ad0d1ecff2c2a74afa114d490616d24cd9d6
ValenSampayo <49347551+ValenSampayo@users.noreply.github.com>
2019-07-20 17:02:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+//Alfabeto//
+let a =".-";
+let b ="-...";
+let c ="-.-.";
+let d ="-..";
+let e =".";
+let f ="..-.";
+let g ="--.";
+let h ="....";
+let i ="..";
+let j =".---";
+let k ="-.-";
+let l =".-..";
+let m ="--";
+let n ="-.";
+let o ="---";
+let p =".--.";
+let q ="--.-";
+let r =".-.";
+let s ="...";
+let t ="-";
+let u ="..-";
+let v ="...-";
+let w =".--";
+let x ="-..-";
+let y ="-.--";
+let z ="--..";
+//Numeros//
+let 0 ="-----";
+let 1 =".----";
+let 2 ="..---";
+let 3 ="...--";
+let 4 ="....-";
+let 5 =".....";
+let 6 ="-....";
+let 7 ="--...";
+let 8 ="---..";
+let 9 ="----.";
+//Simbolos//
+let . = ".-.-.-";
+let , = "--..--";
+let : = "---...";
+let ´ = ".----.";
+let - = "-....-";
+let / = "-..-.";
+let ( = "-..-.";
+let ) = "-.--.-";
+let " = ".-..-.";
+let = ="-...-";
+let traduccion = document.getElementById('trad');
+function traduction(){
+  if (trad.charAt(0) == a ){
+
+  }
+}
