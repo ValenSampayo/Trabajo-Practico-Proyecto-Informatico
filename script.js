ff60c8ec6e862a7a60e23103bb48defbe97510ef
ValenSampayo <49347551+ValenSampayo@users.noreply.github.com>
2019-08-07 10:22:24 -0300

    TP terminado

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
         function convert(){
-            var letters = [ ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
-            var morseLetters = [ " / ", ". -", "- . . .", "- . - .", "- . .", ".", ". . - .", "- - .", ". . . .", ". .", ". - - -", "- . -", ". - . .",  "- -", "- .", "- - -", ". - - .", "- - . -", ". - .", ". . .", "-", ". . -", ". . . -", ". - -", "- . . -", "- . - -", "- - . .", ". - - - -", ". . - - -", ". . . - -", ". . . . -", ". . . . .", "- . . . .", "- - . . .", "- - - . .", "- - - - .", "- - - - -" ];            
-            var textToChange = document.getElementById("textToConvert").value;
+            var letras = [ ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
+            var morse = [ " / ", ". -", "- . . .", "- . - .", "- . .", ".", ". . - .", "- - .", ". . . .", ". .", ". - - -", "- . -", ". - . .",  "- -", "- .", "- - -", ". - - .", "- - . -", ". - .", ". . .", "-", ". . -", ". . . -", ". - -", "- . . -", "- . - -", "- - . .", ". - - - -", ". . - - -", ". . . - -", ". . . . -", ". . . . .", "- . . . .", "- - . . .", "- - - . .", "- - - - .", "- - - - -" ];
+            var textoatraducir = document.getElementById("textToConvert").value;
             var newText = "";
-            for (var i = 0; i < textToChange.length; i++) {
+            for (var i = 0; i < textoatraducir.length; i++) {
                 for (var j = 0; j < 37; j++) {
-                    if (textToChange[i].toLowerCase() == letters[j]) {
-                        newText += morseLetters[j];
+                    if (textoatraducir[i].toLowerCase() == letras[j]) {
+                        newText += morse[j];
                         newText += "\xa0\xa0\xa0";
                         break;
-                    }		
+                    }
                 }
             }
         document.getElementById("newText").innerHTML = newText;
